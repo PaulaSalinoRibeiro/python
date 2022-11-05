c0c068cb63a806098b2dbb7d739bc80d324f42e6
Paula <paulasalinor@gmail.com>
2022-11-05 15:48:25 -0300

    feat: get_next_page

--- book_crawlers/crawlers_book.py
+++ book_crawlers/crawlers_book.py
@@ -1,7 +1,8 @@
 from parsel import Selector
 import requests
+import json
 
-BASE_URL = 'http://books.toscrape.com/'
+BASE_URL = "http://books.toscrape.com/"
 
 def home_page(url):
   try:
@@ -25,9 +26,10 @@ def details_page(path):
  
   image = book.css("div.item img::attr(src)").get()
   title = book.css(".product_main h1::text").get()
+  price = book.css(".price_color::text").get()
   description = book.css(".product_page > p::text").get()
  
-  return { "image": image, "title": title, "description": description }
+  return { "image": image, "title": title, "price": price,  "description": description }
 
 
 def get_all_details_by_page():
@@ -41,7 +43,34 @@ def get_all_details_by_page():
   return books_details
 
 
-if __name__ == '__main__':
+def get_next_page():
+  next_page = "/"
+  all_books = []
+  
+  while next_page:
+    try:
+      content = home_page(BASE_URL + next_page)
+      books = get_all_details_by_page()
+      all_books.extend(books)
+      next_page = Selector(content).css("li.next > a::attr(href)").get()
+      if not next_page.__contains__("catalogue"):
+        next_page = "/catalogue/" + next_page
+    except AttributeError:
+      break
+    
+  return all_books
 
-  print(get_all_details_by_page())
+
+def generate_file(data):
+  with open("books.json", "w") as file:
+    json.dump(data, file)
+
+
+
+if __name__ == '__main__':
+  paths = details_links()
+  bbt_of_books = get_next_page()
+  print(len(bbt_of_books))
+  
+
  
